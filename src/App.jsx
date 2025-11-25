0a112f00ee7d3799234f487f5cd2850f013586aa
Arjun Raj <arjunrajrd2001@gmail.com>
2025-11-25 12:04:00 +0530

    react project

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+import Logo from './images/logo192.png'
+function App(){
+  return(
+    <>
+    
+    <Demo />
+    </>
+  )
+}
+
+
+function Demo(){
+  return(
+    <>
+    <h1>welcome</h1>
+    <h2>This is Another Function</h2>
+    <img src={Logo} alt="" />
+     <img src={Logo} alt="" />
+    
+    </>
+  )
+}
+
+export default App
+
